7415098c3c03ac7f3b1d201ce7cc86bd1e2eab8d
Karlelisa <karlelisaw@gmail.com>
2019-11-03 21:51:42 -0500

    updated page 4 words selection css and page 5 story results page

--- Mad-Libs/assets/javascript/game.js
+++ Mad-Libs/assets/javascript/game.js
@@ -114,6 +114,11 @@ $(document).ready(function () {
         $("#myModal").modal();
     });
 
+    //At the end of the game when the play again button is clicked, the game will reset to the Select a Theme page
+    $(document).on("click", "#play-again", function () {
+        // reset();
+    });
+
     // Get the words
     getNouns();
     getVerbs();
@@ -127,6 +132,89 @@ $(document).ready(function () {
     // User select the words from the drop-downs
 
     // Submit
+
+    //local storage for words submitted on the Select Your Words Page
+    $("#words-submit").click(function () {
+        console.log('submit', this)
+        let mine = $('#noun option:selected').val()// this is the selected item
+        //create an object to store the item
+        let selected = { noun: "Rodney", selected: mine }
+        // var input = document.getElementById('noun');
+        console.log($('#noun option:selected').val());
+        localStorage.setItem('selected', JSON.stringify(selected));
+    });
+
+
+
+
+
+
+    // Initialize Firebase
+
+    /*    let config = {
+        apiKey: "AIzaSyDTXMa39PExEZ3nSd4-pMZo9fQMHuuQWjA",
+        authDomain: "karlelisas-test-project.firebaseapp.com",
+        databaseURL: "https://karlelisas-test-project.firebaseio.com",
+        storageBucket: "karlelisas-test-project.appspot.com"
+    };
+ */
+
+    //firebase.initializeApp(config);
+
+    // Create a variable to reference the database.
+    // let database = firebase.database();
+
+
+    //Click function for Mad Libs Movie Theme
+    $(document).on("click", "#movie-theme", function () {
+
+        getNouns();
+        getVerbs();
+        getAdjs();
+        getAdvs();
+
+    });
+
+    //Click function for Mad Libs Music Theme
+    $(document).on("click", "#music-theme", function () {
+
+        getNouns();
+        getVerbs();
+        getAdjs();
+        getAdvs();
+
+    });
+
+
+
+
+    //Click function for Mad Libs Pop Culture Theme
+    $(document).on("click", "#pop-culture-theme", function () {
+
+        getNouns();
+        getVerbs();
+        getAdjs();
+        getAdvs();
+
+    });
+
+
+    ////Click function for Mad Libs TV Shows Theme
+    $(document).on("click", "#tv-show-theme", function () {
+
+        getNouns();
+        getVerbs();
+        getAdjs();
+        getAdvs();
+
+    });
+
+
+
+
+
+
+
 })
 
 
