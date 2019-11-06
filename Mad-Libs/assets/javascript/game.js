5b40662de8775d422afd714d749715189b488b16
Karlelisa <karlelisaw@gmail.com>
2019-11-06 02:14:49 -0500

    adding hide and show function

--- Mad-Libs/assets/javascript/game.js
+++ Mad-Libs/assets/javascript/game.js
@@ -1,5 +1,6 @@
 
 
+
 // This is the js for page 4, when user input their choices, and make selection
 // Get 5 nouns /words from words api
 // curl 'https://wordsapiv1.p.mashape.com/words/?partOfSpeech=adverb&random=true' -H "X-Mashape-Key: c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea"
@@ -31,7 +32,6 @@ function getNouns() {
 
     for (let i = 0; i < 5; i++) {
         $.ajax({
-            method: "POST",
             beforeSend: function (request) {
                 request.setRequestHeader("X-Mashape-Key", "c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea");
             },
@@ -51,7 +51,6 @@ function getVerbs() {
 
     for (let i = 0; i < 5; i++) {
         $.ajax({
-            method: "POST",
             beforeSend: function (request) {
                 request.setRequestHeader("X-Mashape-Key", "c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea");
             },
@@ -71,7 +70,6 @@ function getAdjs() {
 
     for (let i = 0; i < 5; i++) {
         $.ajax({
-            method: "POST",
             beforeSend: function (request) {
                 request.setRequestHeader("X-Mashape-Key", "c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea");
             },
@@ -91,7 +89,6 @@ function getAdvs() {
 
     for (let i = 0; i < 5; i++) {
         $.ajax({
-            method: "POST",
             beforeSend: function (request) {
                 request.setRequestHeader("X-Mashape-Key", "c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea");
             },
@@ -115,10 +112,9 @@ $(document).ready(function () {
     });
 
     //At the end of the game when the play again button is clicked, the game will reset to the Select a Theme page
-    $(document).on("click", "#play-again", function () {
+    $(document).on("click", "#play-again-btn", function () {
         // reset();
     });
-
     // Get the words
     getNouns();
     getVerbs();
@@ -147,36 +143,82 @@ $(document).ready(function () {
 
 
 
+    //This is the js for page 3, when the user selects one of the four themes
 
-
-    // Initialize Firebase
-
-    /*    let config = {
-        apiKey: "AIzaSyDTXMa39PExEZ3nSd4-pMZo9fQMHuuQWjA",
-        authDomain: "karlelisas-test-project.firebaseapp.com",
-        databaseURL: "https://karlelisas-test-project.firebaseio.com",
-        storageBucket: "karlelisas-test-project.appspot.com"
+    // Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyAZbkSjLOrmZFERAxfzs7AHI5-ht5xbT7k",
+        authDomain: "madlibs-f325f.firebaseapp.com",
+        databaseURL: "https://madlibs-f325f.firebaseio.com",
+        projectId: "madlibs-f325f",
+        storageBucket: "madlibs-f325f.appspot.com",
+        messagingSenderId: "702529571921",
+        appId: "1:702529571921:web:f073aeea048c108bf8fc70",
+        measurementId: "G-PDRGRKZPXH"
     };
- */
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig)
 
-    //firebase.initializeApp(config);
+    // Connect a story from the database to the appropriate theme picture. 
 
-    // Create a variable to reference the database.
-    // let database = firebase.database();
+    let database = firebase.database();
+
+    //let storyRef = database.ref("/madlibs-f325f");
+
+    //Click function for Mad Libs Movie Theme
+    /*     $("#movie-theme").on("click", function (event) {
+            event.preventDefault();
+    
+            database.ref("/movies").on("value", function (childSnapshot) { */
+    // storing the snapshot.val() in a variable for convenience
+    /*  let movies = [];
+     let movieName = childSnapshot.val().name;
+     let movieStory = childSnapshot.val().story;
+
+     console.log('snap is:', childSnapshot.val());
+*/
+    // for (let i = 0; i < movies.length; i++) {
+    // This sets the movie theme image with a random movie name array. For each movie name.
+    /*     console.log($(this))
+        let movieOptions = movies[Math.floor(Math.random() * movies.length)];
+        $(this).attr("data-movievalue", movieOptions);
+    }
+    // */
+    //let movieValue = ($(this).attr("data-movievalue"));
+    //pull random movie name along with its story
+    //push the nouns, verbs, adjectives and adverbs to the page 4 words selection page 
+
+
+    getNouns();
+    getVerbs();
+    getAdjs();
+    getAdvs();
+
+
+
+    //hide the story
+    //$('moviestory').hide();??????? I know this is wrong.
+
+
+    /* 
+            });
+        }); */
+
+
 
 
     //Click function for Mad Libs Movie Theme
-    $(document).on("click", "#movie-theme", function () {
-
-        getNouns();
-        getVerbs();
-        getAdjs();
-        getAdvs();
-
-    });
-
+    /*    $(document).on("click", "#movie-theme", function () {
+   
+           getNouns();
+           getVerbs();
+           getAdjs();
+           getAdvs();
+   
+       });
+    */
     //Click function for Mad Libs Music Theme
-    $(document).on("click", "#music-theme", function () {
+    /* $(document).on("click", "#music-theme", function () {
 
         getNouns();
         getVerbs();
@@ -184,30 +226,30 @@ $(document).ready(function () {
         getAdvs();
 
     });
-
+ */
 
 
 
     //Click function for Mad Libs Pop Culture Theme
-    $(document).on("click", "#pop-culture-theme", function () {
-
-        getNouns();
-        getVerbs();
-        getAdjs();
-        getAdvs();
-
-    });
-
+    /*  $(document).on("click", "#pop-culture-theme", function () {
+ 
+         getNouns();
+         getVerbs();
+         getAdjs();
+         getAdvs();
+ 
+     });
+  */
 
     ////Click function for Mad Libs TV Shows Theme
-    $(document).on("click", "#tv-show-theme", function () {
-
-        getNouns();
-        getVerbs();
-        getAdjs();
-        getAdvs();
-
-    });
+    /*  $(document).on("click", "#tv-show-theme", function () {
+ 
+         getNouns();
+         getVerbs();
+         getAdjs();
+         getAdvs();
+ 
+     }); */
 
 
 
