e49e53a65a0c543b72c4fa944a012421318daafd
daniellekeefe <daniellekeefe@users.noreply.github.com>
2019-11-06 20:50:49 -0500

    Add files via upload

---
+++ Project-Madlibs-Karlelisa-Branch/Mad-Libs/assets/javascript/category.js
@@ -0,0 +1,32 @@
+
+$(document).ready(() => {
+    $(".select-your-words").hide();
+
+    let stories = ["movies", "pop-culture", "songs", "tv"];
+
+    //******This code needs to be fixed. Its appending more than 4 images********
+    for (let i = 0; i < 1; i++) {
+        // $('.theme-container').append(`<img src='assets/images/movies-mad-libs.jpg' class='theme-image'category=${stories[i]}>`)
+        $('.theme-container').append(`<img src='assets/images/movies-mad-libs.jpg' class='theme-image' category=${stories[i]}>` +
+            `<img src='assets/images/music-libs.jpg' class='theme-image' category=${stories[i]}>` +
+            `<img src='assets/images/pop-culture-wihte-background.jpg' class='theme-image'category=${stories[i]}>` +
+            `<img src='assets/images/tv-show-mad-libs.png' class='theme-image' category=${stories[i]}>`)
+
+    }
+
+    //When the user selects a category, hide all theme images
+    $(".theme-container").on("click", ".theme-image", function () {
+        story_name = $(this).attr("category")
+        $(".theme-container").hide();
+
+        //Show the words selector form 
+        $(".select-your-words").show();
+    });
+
+
+
+
+});
+
+
+
