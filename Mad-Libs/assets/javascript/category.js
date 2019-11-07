1a17b4c0a90072290801bae00f46f3085add4134
Karlelisa <karlelisaw@gmail.com>
2019-11-07 01:44:22 -0500

    updated css and category.js and game.js

--- Mad-Libs/assets/javascript/category.js
+++ Mad-Libs/assets/javascript/category.js
@@ -3,61 +3,24 @@ $(document).ready(() => {
   $(".select-your-words").hide();
 
   let stories = ["movies", "pop-culture", "songs", "tv"];
-  let stories2 = [
-    {
-      story: "movies",
-      src: "assets/images/movies-mad-libs.jpg"
-    },
-    {
-      story: "songs",
-      src: "assets/images/music-libs.jpg"
-    },
-    {
-      story: "pop-culture",
-      src: "assets/images/pop-culture-wihte-background.jpg"
-    },
-    {
-      story: "tv",
-      src: "assets/images/tv-show-mad-libs.png"
-    }
-  ];
 
-  for (let i = 0; i < stories2.length; i++) {
-    console.log("story 2");
-    console.log(stories2[i]);
+  for (let i = 0; i < 1; i++) {
+    // $('.theme-container').append(`<img src='assets/images/movies-mad-libs.jpg' class='theme-image'category=${stories[i]}>`)
     $(".theme-container").append(
-      `<img src=${stories2[i].src} class='theme-image'category=${stories2[i].story}>`
+      `<a href="./mad-libs-word-selector.html"><img src='assets/images/movies-mad-libs.jpg' class='theme-image' category=${stories[i]}></a>` +
+        `<a href="./mad-libs-word-selector.html"><img src='assets/images/music-libs.jpg' class='theme-image' category=${stories[i]}></a>` +
+        `<a href="./mad-libs-word-selector.html"><img src='assets/images/pop-culture-wihte-background.jpg' class='theme-image'category=${stories[i]}></a>` +
+        `<a href="./mad-libs-word-selector.html"><img src='assets/images/tv-show-mad-libs.png' class='theme-image' category=${stories[i]}></a>`
     );
   }
-
-  /*   $(".theme-container"stories2[i].src);
-  }
-
-  //******This code needs to be fixed. Its appending more than 4 images********
-  //   for (let i = 0; i < stories.length; i++) {
-  // $('.theme-container').append(`<img src='assets/images/movies-mad-libs.jpg' class='theme-image'category=${stories[i]}>`)
-  /*   $(".theme-container").append(
-      `<img src='assets/images/movies-mad-libs.jpg' class='theme-image' category=${stories[i]}>` +
-        `<img src='assets/images/music-libs.jpg' class='theme-image' category=${stories[i]}>` +
-        `<img src='assets/images/pop-culture-wihte-background.jpg' class='theme-image'category=${stories[i]}>` +
-        `<img src='assets/images/tv-show-mad-libs.png' class='theme-image' category=${stories[i]}>`
-    );
-  } */
 
   //When the user selects a category, hide all theme images
   $(".theme-container").on("click", ".theme-image", function(event) {
     story_name = $(this).attr("category");
-    console.log();
+    console.log($(this).attr("category"));
     $(".theme-container").hide();
 
     //Show the words selector form page
     $(".select-your-words").show();
   });
-
-  //https://jsbin.com/yinudid/1/edit?html,output
-  ScrollReveal({ duration: 1000 });
-
-  ScrollReveal().reveal(".headline");
-  ScrollReveal().reveal(".tagline", { delay: 500 });
-  ScrollReveal().reveal(".punchline", { delay: 2000 });
 });
--- Mad-Libs/assets/javascript/game.js
+++ Mad-Libs/assets/javascript/game.js
@@ -1,6 +1,3 @@
-
-
-
 // This is the js for page 4, when user input their choices, and make selection
 // Get 5 nouns /words from words api
 // curl 'https://wordsapiv1.p.mashape.com/words/?partOfSpeech=adverb&random=true' -H "X-Mashape-Key: c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea"
@@ -9,7 +6,6 @@ let nounString = "?partOfSpeech=noun";
 let verbString = "?partOfSpeech=verb";
 let adjectiveString = "?partOfSpeech=adjective";
 let adverbString = "?partOfSpeech=adverb";
-
 
 let nounUrl = baseUrl + nounString + "&random=true";
 let verbUrl = baseUrl + verbString + "&random=true";
@@ -26,241 +22,170 @@ let verbArray = [];
 let adjArray = [];
 let advArray = [];
 
-
-
 function getNouns() {
+  for (let i = 0; i < 5; i++) {
+    $.ajax({
+      beforeSend: function(request) {
+        request.setRequestHeader(
+          "X-Mashape-Key",
+          "c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea"
+        );
+      },
+      method: "GET",
+      url: nounUrl
+    }).then(function(response) {
+      console.log(response);
+      console.log(response.word);
 
-    for (let i = 0; i < 5; i++) {
-        $.ajax({
-            beforeSend: function (request) {
-                request.setRequestHeader("X-Mashape-Key", "c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea");
-            },
-            method: "GET",
-            url: nounUrl,
-        }).then(function (response) {
-            console.log(response);
-            console.log(response.word);
-
-            $("#noun").append('<option class="noun" id="noun1">' + response.word + '</option>');
-
-        })
-    }
+      $("#noun").append(
+        '<option class="noun" id="noun1">' + response.word + "</option>"
+      );
+    });
+  }
 }
 
 function getVerbs() {
+  for (let i = 0; i < 5; i++) {
+    $.ajax({
+      beforeSend: function(request) {
+        request.setRequestHeader(
+          "X-Mashape-Key",
+          "c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea"
+        );
+      },
+      method: "GET",
+      url: verbUrl
+    }).then(function(response) {
+      console.log(response);
+      console.log(response.word);
 
-    for (let i = 0; i < 5; i++) {
-        $.ajax({
-            beforeSend: function (request) {
-                request.setRequestHeader("X-Mashape-Key", "c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea");
-            },
-            method: "GET",
-            url: verbUrl,
-        }).then(function (response) {
-            console.log(response);
-            console.log(response.word);
-
-            $("#verb").append('<option class="verb">' + response.word + '</option>');
-
-        })
-    }
+      $("#verb").append('<option class="verb">' + response.word + "</option>");
+    });
+  }
 }
 
 function getAdjs() {
+  for (let i = 0; i < 5; i++) {
+    $.ajax({
+      beforeSend: function(request) {
+        request.setRequestHeader(
+          "X-Mashape-Key",
+          "c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea"
+        );
+      },
+      method: "GET",
+      url: adjectiveUrl
+    }).then(function(response) {
+      console.log(response);
+      console.log(response.word);
 
-    for (let i = 0; i < 5; i++) {
-        $.ajax({
-            beforeSend: function (request) {
-                request.setRequestHeader("X-Mashape-Key", "c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea");
-            },
-            method: "GET",
-            url: adjectiveUrl,
-        }).then(function (response) {
-            console.log(response);
-            console.log(response.word);
-
-            $("#adjective").append('<option class="adjective">' + response.word + '</option>');
-
-        })
-    }
+      $("#adjective").append(
+        '<option class="adjective">' + response.word + "</option>"
+      );
+    });
+  }
 }
 
 function getAdvs() {
+  for (let i = 0; i < 5; i++) {
+    $.ajax({
+      beforeSend: function(request) {
+        request.setRequestHeader(
+          "X-Mashape-Key",
+          "c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea"
+        );
+      },
+      method: "GET",
+      url: adverbUrl
+    }).then(function(response) {
+      console.log(response);
+      console.log(response.word);
 
-    for (let i = 0; i < 5; i++) {
-        $.ajax({
-            beforeSend: function (request) {
-                request.setRequestHeader("X-Mashape-Key", "c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea");
-            },
-            method: "GET",
-            url: adverbUrl,
-        }).then(function (response) {
-            console.log(response);
-            console.log(response.word);
-
-            $("#adverb").append('<option class="adverb">' + response.word + '</option>');
-
-        })
-    }
+      $("#adverb").append(
+        '<option class="adverb">' + response.word + "</option>"
+      );
+    });
+  }
 }
-$(document).ready(function () {
 
-    // Cited: https://www.w3schools.com/bootstrap/tryit.asp?filename=trybs_ref_js_modal2&stacked=h 
-    /* The toggle start and sign up button */
-    $("#start-btn").click(function () {
-        $("#myModal").modal();
-    });
+$(document).ready(function() {
+  // Cited: https://www.w3schools.com/bootstrap/tryit.asp?filename=trybs_ref_js_modal2&stacked=h
+  /* The toggle start and sign up button */
+  $("#start-btn").click(function() {
+    $("#myModal").modal();
+  });
 
-    //At the end of the game when the play again button is clicked, the game will reset to the Select a Theme page
-    $(document).on("click", "#play-again-btn", function () {
-        // reset();
-    });
-    // Get the words
-    getNouns();
-    getVerbs();
-    getAdjs();
-    getAdvs();
-    //getLocations();
-    //...
-    //writeNouns();
-    // Put the words into html form to the correct input.
+  //At the end of the game when the play again button is clicked, the game will reset to the Select a Theme page
+  $(document).on("click", "#play-again-btn", function() {
+    // reset();
+  });
+  // Get the words
+  getNouns();
+  getVerbs();
+  getAdjs();
+  getAdvs();
+  //getLocations();
+  //...
+  //writeNouns();
+  // Put the words into html form to the correct input.
 
-    // User select the words from the drop-downs
+  // User select the words from the drop-downs
 
-    // Submit
+  // Submit
 
-    //local storage for words submitted on the Select Your Words Page
-    $("#words-submit").click(function () {
-        console.log('submit', this)
-        let mine = $('#noun option:selected').val()// this is the selected item
-        //create an object to store the item
-        let selected = { noun: "Rodney", selected: mine }
-        // var input = document.getElementById('noun');
-        console.log($('#noun option:selected').val());
-        localStorage.setItem('selected', JSON.stringify(selected));
-    });
+  //local storage for words submitted on the Select Your Words Page
+  $("#words-submit").click(function() {
+    console.log("submit", this);
+    let mine = $("#noun option:selected").val(); // this is the selected item
+    //create an object to store the item
+    let selected = { noun: "Rodney", selected: mine };
+    // var input = document.getElementById('noun');
+    console.log($("#noun option:selected").val());
+    localStorage.setItem("selected", JSON.stringify(selected));
+  });
 
+  //This is the js for page 3, when the user selects one of the four themes
 
+  // Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAZbkSjLOrmZFERAxfzs7AHI5-ht5xbT7k",
+    authDomain: "madlibs-f325f.firebaseapp.com",
+    databaseURL: "https://madlibs-f325f.firebaseio.com",
+    projectId: "madlibs-f325f",
+    storageBucket: "madlibs-f325f.appspot.com",
+    messagingSenderId: "702529571921",
+    appId: "1:702529571921:web:f073aeea048c108bf8fc70",
+    measurementId: "G-PDRGRKZPXH"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
+  // Connect a story from the database to the appropriate theme picture.
 
-    //This is the js for page 3, when the user selects one of the four themes
+  let database = firebase.database();
 
-    // Firebase configuration
-    var firebaseConfig = {
-        apiKey: "AIzaSyAZbkSjLOrmZFERAxfzs7AHI5-ht5xbT7k",
-        authDomain: "madlibs-f325f.firebaseapp.com",
-        databaseURL: "https://madlibs-f325f.firebaseio.com",
-        projectId: "madlibs-f325f",
-        storageBucket: "madlibs-f325f.appspot.com",
-        messagingSenderId: "702529571921",
-        appId: "1:702529571921:web:f073aeea048c108bf8fc70",
-        measurementId: "G-PDRGRKZPXH"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig)
+  //let storyRef = database.ref("/madlibs-f325f");
 
-    // Connect a story from the database to the appropriate theme picture. 
-
-    let database = firebase.database();
-
-    //let storyRef = database.ref("/madlibs-f325f");
-
-    //Click function for Mad Libs Movie Theme
-    /*     $("#movie-theme").on("click", function (event) {
+  //Click function for Mad Libs Movie Theme
+  /*  $("#movie-theme").on("click", function (event) {
             event.preventDefault();
     
-            database.ref("/movies").on("value", function (childSnapshot) { */
-    // storing the snapshot.val() in a variable for convenience
-    /*  let movies = [];
+            database.ref("/movies").on("value", function (childSnapshot) {  */
+  // storing the snapshot.val() in a variable for convenience
+  /*   let movies = [];
      let movieName = childSnapshot.val().name;
      let movieStory = childSnapshot.val().story;
 
-     console.log('snap is:', childSnapshot.val());
-*/
-    // for (let i = 0; i < movies.length; i++) {
-    // This sets the movie theme image with a random movie name array. For each movie name.
-    /*     console.log($(this))
+     console.log('snap is:', childSnapshot.val()); */
+
+  // for (let i = 0; i < movies.length; i++) {
+  // This sets the movie theme image with a random movie name array. For each movie name.
+  /*     console.log($(this))
         let movieOptions = movies[Math.floor(Math.random() * movies.length)];
         $(this).attr("data-movievalue", movieOptions);
     }
     // */
-    //let movieValue = ($(this).attr("data-movievalue"));
-    //pull random movie name along with its story
-    //push the nouns, verbs, adjectives and adverbs to the page 4 words selection page 
-
-
-    getNouns();
-    getVerbs();
-    getAdjs();
-    getAdvs();
-
-
-
-    //hide the story
-    //$('moviestory').hide();??????? I know this is wrong.
-
-
-    /* 
-            });
-        }); */
-
-
-
-
-    //Click function for Mad Libs Movie Theme
-    /*    $(document).on("click", "#movie-theme", function () {
-   
-           getNouns();
-           getVerbs();
-           getAdjs();
-           getAdvs();
-   
-       });
-    */
-    //Click function for Mad Libs Music Theme
-    /* $(document).on("click", "#music-theme", function () {
-
-        getNouns();
-        getVerbs();
-        getAdjs();
-        getAdvs();
-
-    });
- */
-
-
-
-    //Click function for Mad Libs Pop Culture Theme
-    /*  $(document).on("click", "#pop-culture-theme", function () {
- 
-         getNouns();
-         getVerbs();
-         getAdjs();
-         getAdvs();
- 
-     });
-  */
-
-    ////Click function for Mad Libs TV Shows Theme
-    /*  $(document).on("click", "#tv-show-theme", function () {
- 
-         getNouns();
-         getVerbs();
-         getAdjs();
-         getAdvs();
- 
-     }); */
-
-
-
-
-
-
-
-})
-
-
-
-// $(window).on("load", function () {
-
-// });
+  //let movieValue = ($(this).attr("data-movievalue"));
+  //pull random movie name along with its story
+  //push the nouns, verbs, adjectives and adverbs to the page 4 words selection page
+});
