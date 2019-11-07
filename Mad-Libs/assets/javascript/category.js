effd61d3e06984b1bd0ff838d13f09557f1f57ae
Karlelisa <karlelisaw@gmail.com>
2019-11-06 19:54:47 -0500

    corrected the theme image for loop to only append one image per theme in category.js

--- Mad-Libs/assets/javascript/category.js
+++ Mad-Libs/assets/javascript/category.js
@@ -1,32 +1,63 @@
-
 $(document).ready(() => {
-    $(".select-your-words").hide();
+  //hide the words selector form page
+  $(".select-your-words").hide();
 
-    let stories = ["movies", "pop-culture", "songs", "tv"];
-
-    //******This code needs to be fixed. Its appending more than 4 images********
-    for (let i = 0; i < stories.length; i++) {
-        // $('.theme-container').append(`<img src='assets/images/movies-mad-libs.jpg' class='theme-image'category=${stories[i]}>`)
-        $('.theme-container').append(`<img src='assets/images/movies-mad-libs.jpg' class='theme-image' category=${stories[i]}>` +
-            `<img src='assets/images/music-libs.jpg' class='theme-image' category=${stories[i]}>` +
-            `<img src='assets/images/pop-culture-wihte-background.jpg' class='theme-image'category=${stories[i]}>` +
-            `<img src='assets/images/tv-show-mad-libs.png' class='theme-image' category=${stories[i]}>`)
-
+  let stories = ["movies", "pop-culture", "songs", "tv"];
+  let stories2 = [
+    {
+      story: "movies",
+      src: "assets/images/movies-mad-libs.jpg"
+    },
+    {
+      story: "songs",
+      src: "assets/images/music-libs.jpg"
+    },
+    {
+      story: "pop-culture",
+      src: "assets/images/pop-culture-wihte-background.jpg"
+    },
+    {
+      story: "tv",
+      src: "assets/images/tv-show-mad-libs.png"
     }
+  ];
 
-    //When the user selects a category, hide all theme images
-    $(".theme-container").on("click", ".theme-image", function () {
-        story_name = $(this).attr("category")
-        $(".theme-container").hide();
+  for (let i = 0; i < stories2.length; i++) {
+    console.log("story 2");
+    console.log(stories2[i]);
+    $(".theme-container").append(
+      `<img src=${stories2[i].src} class='theme-image'category=${stories2[i].story}>`
+    );
+  }
 
-        //Show the words selector form 
-        $(".select-your-words").show();
-    });
+  /*   $(".theme-container"stories2[i].src);
+  }
 
+  //******This code needs to be fixed. Its appending more than 4 images********
+  //   for (let i = 0; i < stories.length; i++) {
+  // $('.theme-container').append(`<img src='assets/images/movies-mad-libs.jpg' class='theme-image'category=${stories[i]}>`)
+  /*   $(".theme-container").append(
+      `<img src='assets/images/movies-mad-libs.jpg' class='theme-image' category=${stories[i]}>` +
+        `<img src='assets/images/music-libs.jpg' class='theme-image' category=${stories[i]}>` +
+        `<img src='assets/images/pop-culture-wihte-background.jpg' class='theme-image'category=${stories[i]}>` +
+        `<img src='assets/images/tv-show-mad-libs.png' class='theme-image' category=${stories[i]}>`
+    );
+  } */
 
+  //When the user selects a category, hide all theme images
+  $(".theme-container").on("click", ".theme-image", function(event) {
+    story_name = $(this).attr("category");
+    console.log();
+    $(".theme-container").hide();
 
+    //Show the words selector form page
+    $(".select-your-words").show();
+  });
 
+  //https://jsbin.com/yinudid/1/edit?html,output
+  ScrollReveal({ duration: 1000 });
+
+  ScrollReveal().reveal(".headline");
+  ScrollReveal().reveal(".tagline", { delay: 500 });
+  ScrollReveal().reveal(".punchline", { delay: 2000 });
 });
-
-
-
